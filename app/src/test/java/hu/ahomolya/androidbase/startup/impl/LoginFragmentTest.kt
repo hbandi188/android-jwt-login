cf88efb75fbef2ed615da8d3271dbcf3461a01a6
Andras Homolya <945781-hbandi188@users.noreply.gitlab.com>
2021-08-26 22:47:46 +0200

    add UI elements to login page

--- app/src/test/java/hu/ahomolya/androidbase/startup/impl/LoginFragmentTest.kt
+++ app/src/test/java/hu/ahomolya/androidbase/startup/impl/LoginFragmentTest.kt
@@ -1,5 +1,8 @@
 package hu.ahomolya.androidbase.startup.impl
 
+import android.text.InputType.TYPE_CLASS_TEXT
+import android.text.InputType.TYPE_TEXT_VARIATION_PASSWORD
+import android.widget.EditText
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
@@ -8,6 +11,9 @@ import androidx.test.ext.junit.runners.AndroidJUnit4
 import dagger.hilt.android.testing.HiltAndroidTest
 import hu.ahomolya.androidbase.BaseTest
 import hu.ahomolya.androidbase.R
+import io.kotest.matchers.collections.shouldContain
+import io.kotest.matchers.nulls.shouldNotBeNull
+import io.kotest.matchers.shouldBe
 import org.junit.Test
 import org.junit.runner.RunWith
 
@@ -19,6 +25,42 @@ class LoginFragmentTest : BaseTest() {
         activityRule.scenario.onActivity {
             onView(withId(R.id.page_title)).check(matches(withEffectiveVisibility(VISIBLE)))
             onView(withId(R.id.page_title)).check(matches(withText(R.string.login_title)))
+        }
+    }
+
+    @Test
+    fun `login page has username field`() {
+        activityRule.scenario.onActivity {
+            onView(withId(R.id.username_field)).check(matches(withEffectiveVisibility(VISIBLE)))
+            onView(withId(R.id.username_field)).check(matches(withHint(R.string.username)))
+
+            val usernameField = it.findViewById<EditText>(R.id.username_field)
+            val autofillHints = usernameField.autofillHints
+            autofillHints.shouldNotBeNull()
+            autofillHints shouldContain "username"
+            usernameField.inputType shouldBe TYPE_CLASS_TEXT
+        }
+    }
+
+    @Test
+    fun `login page has password field`() {
+        activityRule.scenario.onActivity {
+            onView(withId(R.id.password_field)).check(matches(withEffectiveVisibility(VISIBLE)))
+            onView(withId(R.id.password_field)).check(matches(withHint(R.string.password)))
+
+            val usernameField = it.findViewById<EditText>(R.id.password_field)
+            val autofillHints = usernameField.autofillHints
+            autofillHints.shouldNotBeNull()
+            autofillHints shouldContain "password"
+            usernameField.inputType shouldBe (TYPE_CLASS_TEXT or TYPE_TEXT_VARIATION_PASSWORD)
+        }
+    }
+
+    @Test
+    fun `login page should have login button`() {
+        activityRule.scenario.onActivity {
+            onView(withId(R.id.login_button)).check(matches(withEffectiveVisibility(VISIBLE)))
+            onView(withId(R.id.login_button)).check(matches(withText(R.string.login_button)))
         }
     }
 }
